dc615238085ae413a157553fd75bb9984614577f
vueling <abdel@vueling.fr>
2022-03-24 15:02:31 +0100

    exo4

--- exo4.php
+++ exo4.php
@@ -10,17 +10,38 @@
 // $resulat = get_prix(100,2,20);
 // echo "le prix total des articles est: $resulat euro  ";
 
-function verifier($x){
-    if($x > 0){
-        echo "le nombre est postif";
+// function verifier($x){
+//     if($x > 0){
+//         echo "le nombre est postif";
+//     }
+    
+    
+//     elseif($x < 0){
+//         echo "le nombre est negatif";
+
+//     }
+//     else if ($x==0) {
+//         echo "il est nul";
+//     }
+// }
+// verifier(0);
+Exercice 2 :
+-Créer une fonction qui prend en entrée 3 valeurs et retourne le nombre le plus grand (echo dans la page web).
+
+function grand($a,$b,$c){
+    if($a > $b){
+        
+
+        return "le nombre le plus grand est $a ";
     }
-    else {
-        echo "il est negatif";
+    else if ($b > $c){
+        return "le nombre le plus grand est $b ";
+    }
+    else{
+        return "le nombre le plus grand est $c ";
     }
 }
-verifier(2);
-
-
+echo grand(4,8,2)
 
 
 ?>
