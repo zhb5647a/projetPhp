a8f578e111fca556d2ec9c17d9e2d149ecd84df8
vueling <abdel@vueling.fr>
2022-03-24 16:34:41 +0100

    swuitch

--- exo4.php
+++ exo4.php
@@ -25,23 +25,92 @@
 //     }
 // }
 // verifier(0);
-Exercice 2 :
--Créer une fonction qui prend en entrée 3 valeurs et retourne le nombre le plus grand (echo dans la page web).
+// :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+// Exercice 2 :
+// -Créer une fonction qui prend en entrée 3 valeurs et retourne le nombre le plus grand (echo dans la page web).
 
-function grand($a,$b,$c){
-    if($a > $b){
+// function grand($a,$b,$c){
+//     if($a > $b){
         
 
-        return "le nombre le plus grand est $a ";
-    }
-    else if ($b > $c){
-        return "le nombre le plus grand est $b ";
-    }
-    else{
-        return "le nombre le plus grand est $c ";
-    }
-}
-echo grand(4,8,2)
+//         return "le nombre le plus grand est $a ";
+//     }
+//     else if ($b > $c){
+//         return "le nombre le plus grand est $b ";
+//     }
+//     else{
+//         return "le nombre le plus grand est $c ";
+//     }
+// }
+// echo grand(22,23,19)
+// :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+// Exercice 4:
+// -Créer une fonction qui prend en entrée 1 valeur (l’âge d’un enfant). Ensuite, elle informe de sa catégorie (echo dans la page web) :
+// • "Poussin" de 6 à 7 ans
+// • "Pupille" de 8 à 9 ans
+// • "Minime" de 10 à 11 ans
+// • "Cadet" après 12 ans
+// Bonus : Refaire l’exercice en utilisant le switch case.
 
+// function verifAge($age){
+//     if(  $age >= 6 AND $age <= 7){
+//         echo " catégorie Poussin";
+//     }
+//     else if($age >= 8 AND $age <= 9){
+//         echo " catégorie Pupille";
+//     }
+//     else if($age >= 10 AND $age <= 11){
+//         echo " catégorie Minime";
+//     }
+//     else if ($age >= 12){
+//         echo "categori cadet";
+        
+//     }
+//     else {
+//         echo " est trop jeune";
+//     }
 
+// }
+//  verifAge(4);
+
+ function switchTest($age){
+    switch($age){
+        case $age>=6 AND $age<=7:
+            echo 'est Poussin';
+            break;
+        case $age>=8 AND $age<=9:
+            echo 'est Pupille';
+            break;
+        case $age>=10 AND $age<=11:
+            echo 'est Minime';
+            break;
+        case $age>=12: 
+            echo 'est Cadet';
+            break;
+        default:
+            echo 'est trop jeune';
+            break;
+        }
+    }
+function switchTestV2($age){
+    switch($age){
+        case $age>=12:
+            echo 'est Cadet';
+            break;
+        case $age>=10:
+            echo 'est Minime';
+            break;
+        case $age>=8:
+            echo 'est Pupille';
+            break;
+        case $age>=6: 
+            echo 'est Poussin';
+            break;
+        default:
+            echo 'est trop jeune';
+            break;
+        }
+    }
+
+    switchTestV2(10)
 ?>
