8ac10c5207ba3a38247d05c7bcfdf91575d1d735
vueling <abdel@vueling.fr>
2022-03-24 11:16:33 +0100

    suite

--- exo1.php
+++ exo1.php
@@ -6,6 +6,11 @@
 -Afficher le contenu de la variable (utilisation de la fonction php echo),
 -Créer une variable de type booléen avec pour valeur false,
 -Afficher son type (utilisation de la fonction php gettype) -->
+<!-- :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: -->
+<!-- Exercice 1 :
+-Créer 2 variables $a et $b qui ont pour valeur 12 et 10,
+-Stocker le résultat de l’addition de $a et $b dans une variable $total,
+-Afficher le résultat (utilisez la fonction echo) -->
 
 <?php
 
@@ -15,8 +20,18 @@ echo '<br/>';
 echo gettype($int);
 echo '<br/>';
 $prenom = 'abdel';
+
+echo "mon prenon est : ".$prenom;
+echo '<br/>';
 echo $prenom;
 echo '<br/>';
 $test = true;
 echo gettype($test);
+echo '<br/>';
+$a =12;
+$b =10;
+$total=$a+$b;
+echo '<br/>';
+echo $total;
+
 ?>
